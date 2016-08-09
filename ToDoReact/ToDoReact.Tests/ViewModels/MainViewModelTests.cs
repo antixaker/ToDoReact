3b92349fb7e2cd1fa2bf44f6ec6c9b10fc251211
Olexandr Proskura <proa@m-pc-proa.kyiv.ciklum.net>
2016-08-09 18:48:34 +0300

    Add tests file for MainViewModel

---
+++ ToDoReact/ToDoReact.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ToDoReact.Tests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private MainViewModel _mainViewModel;
+
+    }
+}
+
