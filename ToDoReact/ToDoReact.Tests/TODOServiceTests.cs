606587d7ad8ac53d4cd118342cb87b15c6089369
Olexandr Proskura <proa@m-pc-proa.kyiv.ciklum.net>
2016-08-09 16:25:24 +0300

    Create test for empty TODO list

---
+++ ToDoReact/ToDoReact.Tests/TODOServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.ObjectModel;
+namespace ToDoReact.Tests
+{
+    [TestFixture()]
+    public class TODOServiceTests
+    {
+        [Test]
+        public void GetAll_GotEmptyList_Success()
+        {
+            // Arrange
+            var todoService = new TODOService();
+
+            // Act
+            var todosList = todoService.GetAll();
+            // Assert
+            Assert.AreEqual(new List<TODOModel>(), todosList);
+        }
+    }
+}
+
