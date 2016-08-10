1d0847af39825df403df23b8147a0cd462f3ed58
Olexandr Proskura <proa@m-pc-proa.kyiv.ciklum.net>
2016-08-10 12:46:31 +0300

    Add Moq and rectiveProperty, write tests for TODO items in viewModel

--- ToDoReact/ToDoReact.Tests/ViewModels/MainViewModelTests.cs
+++ ToDoReact/ToDoReact.Tests/ViewModels/MainViewModelTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using NUnit.Framework;
+using Moq;
+using ToDoReact.Services;
+using ToDoReact.Models;
+using System.Collections.Generic;
 
 namespace ToDoReact.Tests
 {
@@ -9,18 +13,30 @@ namespace ToDoReact.Tests
         [SetUp]
         public void SetUp()
         {
-            _mainViewModel = new MainViewModel();
+            _mockTODOService.Setup(x => x.GetAll()).Returns(new List<TODOModel>() { _testModel, _testModel });
+            _mainViewModel = new MainViewModel(_mockTODOService.Object);
         }
 
+        private TODOModel _testModel = new TODOModel();
         private MainViewModel _mainViewModel;
+        private Mock<ITODOService> _mockTODOService = new Mock<ITODOService>();
 
         [Test]
-        public void MainViewModel_HaveNotNullListAfterCreating_True()
+        public void MainViewModel_HaveNotNullListAfterCreation_True()
         {
             // Arrange
             // Act
             // Assert
             Assert.IsNotNull(_mainViewModel.Items);
+        }
+
+        [Test]
+        public void Items_ValuesAsInTodoServiceGetAll_True()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(new List<TODOModel>() { _testModel, _testModel }, _mainViewModel.Items.Value);
         }
     }
 }
---
+++ ToDoReact/ToDoReact/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ToDoReact.Services;
+using System.Collections.Generic;
+using ToDoReact.Models;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+
+namespace ToDoReact
+{
+    public class MainViewModel
+    {
+        private ITODOService _todoService;
+
+        public MainViewModel(ITODOService todoService)
+        {
+            _todoService = todoService;
+            Items.Value = _todoService.GetAll();
+        }
+
+        public ReactiveProperty<List<TODOModel>> Items = new ReactiveProperty<List<TODOModel>>();
+    }
+}
+
