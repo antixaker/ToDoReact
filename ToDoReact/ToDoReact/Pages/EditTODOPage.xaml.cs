e321b147ca7973b0e7d606cee44190cb4b5fbf80
Alexandr Proskura <proa@ciklum.com>
2016-08-29 16:09:40 +0300

    Change base class for EditTODOPage

--- ToDoReact/ToDoReact/Pages/EditTODOPage.xaml.cs
+++ ToDoReact/ToDoReact/Pages/EditTODOPage.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
 
 namespace Pages
 {
-    public partial class EditTODOPage : ContentPage
+    public partial class EditTODOPage : BaseContentPage
     {
         public EditTODOPage()
         {
